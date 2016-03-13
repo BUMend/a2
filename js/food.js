4970c1c71aab66c51bbbfa0d279272651811abfa
EyesiM <302821641@qq.com>
2016-03-13 22:53:41 +0800

    增加五个板块

--- js/food.js
+++ js/food.js
@@ -1,12 +1,31 @@
         window.onload = function() {
             var ChangePhoto = function() {
-                $('.lt').css('height',$('.lt').width());
-                $('.lb').css('height',$('.lt').height());
-                $('.show-right').css('height',$('.show-left').height());
-                console.log( $('.lt').height())
-                $('.rt').css('height',$('.show-right').height()*0.3968);
-                $('.rtl').css('height',$('.show-right').height()-$('.rt').height());
-                $('.rtr').css('height',$('.show-right').height()-$('.rt').height());
+                console.log($('#tf-sl').width());
+                $('#ft-sl').height($('#ft-sl').width()*2);                
+                $('#fo-sl').height($('#fo-sl').width()*2);
+                $('#tf-sl').height($('#tf-sl').width()*2);
+                $('#fun-sl').height($('#fun-sl').width()*2);       
+                $('#st-sl').height($('#st-sl').width()/0.648);  
+                $('#ft-sr').height($('#ft-sl').height());
+                $('#fo-sr').height($('#fo-sl').height());
+                $('#tf-sr').height($('#tf-sl').height());
+                $('#fun-sr').height($('#fun-sl').height());
+                $('#st-sr').height($('#st-sl').height());
+                $('#st-ct').height($('#st-sl').height());
+
+
+
+
+
+
+
+
+                // $('.lt').css('height',$('.show-left').width());
+                // $('.lb').css('height',$('.show-left').width());
+                // $('.show-right').css('height',$('.show-left').height());
+                // $('.rt').css('height',$('.show-right').height()*0.3968);
+                // $('.rtl').css('height',$('.show-right').height()-$('.rt').height());
+                // $('.rtr').css('height',$('.show-right').height()-$('.rt').height());
             }
             ChangePhoto();
             $(window).on('resize',function() {
